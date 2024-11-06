22f7ce019cde883c1d8a196f54991b689cc45c16
Joao Kruse <soyjog@gmail.com>
2024-11-06 22:09:17 +0900

    fix: Made installation easier

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 # setup.py
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 from distutils.core import setup
 from Cython.Build import cythonize
